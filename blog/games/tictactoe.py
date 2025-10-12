0e91a3dbb1a0e5e0f8d433df74b1bf1e1f0a80fe
Yashwanth K R <148343851+yash2006kr@users.noreply.github.com>
2025-10-12 10:00:03 +0530

    Add linkedin profile

--- blog/games/tictactoe.py
+++ blog/games/tictactoe.py
@@ -9,106 +9,80 @@ print('| 4| 5| 6|')
 print(' -- -- --')
 print('| 7| 8| 9|')
 print(' -- -- --')
-_1=_2=_3=_4=_5=_6=_7=_8=_9=" "
-d1={}
-d2={}
-p1=input("Who will play for 'X': ")
-p2=input("Who will play for 'O' : ")
-print('''
-Let's Gooo....
-''')
-p=0
-ps1=[]
-ps2=[]
-while p<10:
-    a=int(input(p1+" Enter the position: "))
-    
-    if a>9 or a<1:
-        print("Max position is 9 bruh!")
-        continue
-    d1[a]="X"
-    Kalla1=False
-    if Kalla1==False:
-        if a not in ps1 and a not in ps2 :
-            if a==1:
-                _1='X'
-            elif a==2:
-                _2='X'
-            elif a==3:
-                _3='X'
-            elif a==4:
-                _4='X'
-            elif a==5:
-                _5='X'
-            elif a==6:
-                _6='X'
-            elif a==7:
-                _7='X'
-            elif a==8:
-                _8='X'
-            elif a==9:
-                _9='X'
-            else:
-                print("Invalid Position")
-            print(' -- -- --')
-            print('| '+_1+'| '+_2+'| '+_3+'|')
-            print(' -- -- --')
-            print('| '+_4+'| '+_5+'| '+_6+'|')
-            print(' -- -- --')
-            print('| '+_7+'| '+_8+'| '+_9+'|')
-            print(' -- -- --') 
-            if ps1==[1,2,3] or ps1==[4,5,6] or ps1==[7,8,9] or ps1==[1,4,7] or ps1==[2,5,8] or ps1==[3,6,9] or ps1==[1,5,9] or ps1==[3,5,7]:
-                print(p1,"won the game, better luck next time",p2)
-                break
-        
-        else:
-            print("Position is already entered")
-            Kalla1=True
-            continue
-    else:
-        pass
-    ps1=list(d1.keys())
-    b=int(input(p2+" Enter the position: "))
-    d2[b]="O"
-    Kalla2=False
-    if Kalla2==False:
-        if b not in ps1 and b not in ps2:
-            if b==1:
-                _1='O'
-            elif b==2:
-                _2='O'
-            elif b==3:
-                _3='O'
-            elif b==4:
-                _4='O'
-            elif b==5:
-                _5='O'
-            elif b==6:
-                _6='O'
-            elif b==7:
-                _7='O'
-            elif b==8:
-                _8='O'
-            elif b==9:
-                _9='O'
-            else:
-                print("Invalid Position")
-            print(' -- -- --')
-            print('| '+_1+'| '+_2+'| '+_3+'|')
-            print(' -- -- --')
-            print('| '+_4+'| '+_5+'| '+_6+'|')
-            print(' -- -- --')
-            print('| '+_7+'| '+_8+'| '+_9+'|')
-            print(' -- -- --')
-            if ps2==[1,2,3] or ps2==[4,5,6] or ps2==[7,8,9] or ps2==[1,4,7] or ps2==[2,5,8] or ps2==[3,6,9] or ps2==[1,5,9] or ps2==[3,5,7]:
-                print(p2,"won the game, better luck next time",p1)
-                break
-        
-        else:
-            print("Position is already entered")
-            Kalla2=True
-            continue
-    else:
-        pass
-    ps2=list(d2.keys())
-    p+=2
+
+board = [" "] * 9
+positions_taken = set()
+
+p1 = input("Who will play for 'X': ")
+p2 = input("Who will play for 'O' : ")
+
+def print_board():
+    print(' -- -- --')
+    print(f'| {board[0]}| {board[1]}| {board[2]}|')
+    print(' -- -- --')
+    print(f'| {board[3]}| {board[4]}| {board[5]}|')
+    print(' -- -- --')
+    print(f'| {board[6]}| {board[7]}| {board[8]}|')
+    print(' -- -- --')
+
+def check_win(mark):
+    win_combos = [
+        [0,1,2], [3,4,5], [6,7,8], # rows
+        [0,3,6], [1,4,7], [2,5,8], # cols
+        [0,4,8], [2,4,6]           # diags
+    ]
+    for combo in win_combos:
+        if all(board[i] == mark for i in combo):
+            return True
+    return False
+
+print("\nLet's Gooo....\n")
+turn = 0
+while turn < 9:
+    # Player 1
+    while True:
+        try:
+            a = int(input(f"{p1} Enter the position (1-9): "))
+            if a < 1 or a > 9:
+                print("Max position is 9 bruh!")
+                continue
+            if a in positions_taken:
+                print("Position is already entered")
+                continue
+            board[a-1] = 'X'
+            positions_taken.add(a)
+            break
+        except ValueError:
+            print("Enter a valid number!")
+    print_board()
+    if check_win('X'):
+        print(f"{p1} won the game, better luck next time {p2}")
+        break
+    turn += 1
+    if turn == 9:
+        print("It's a draw!")
+        break
+
+    # Player 2
+    while True:
+        try:
+            b = int(input(f"{p2} Enter the position (1-9): "))
+            if b < 1 or b > 9:
+                print("Max position is 9 bruh!")
+                continue
+            if b in positions_taken:
+                print("Position is already entered")
+                continue
+            board[b-1] = 'O'
+            positions_taken.add(b)
+            break
+        except ValueError:
+            print("Enter a valid number!")
+    print_board()
+    if check_win('O'):
+        print(f"{p2} won the game, better luck next time {p1}")
+        break
+    turn += 1
+    if turn == 9:
+        print("It's a draw!")
+        break
